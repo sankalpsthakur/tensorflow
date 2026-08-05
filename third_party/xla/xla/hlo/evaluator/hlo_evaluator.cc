61f859e46c7bf088a385b084d64d3f7cbc87c1a1
A. Unique TensorFlower <gardener@tensorflow.org>
2026-08-05 00:15:54 -0700

    Transition HloEvaluator and CollectivePipeliner from TuplePointsToAnalysis to HloAliasAnalysis.
    PiperOrigin-RevId: 959471606

--- third_party/xla/xla/hlo/evaluator/hlo_evaluator.cc
+++ third_party/xla/xla/hlo/evaluator/hlo_evaluator.cc
@@ -54,7 +54,7 @@ limitations under the License.
 #include "xla/tsl/platform/status_macros.h"
 #include "xla/array2d.h"
 #include "xla/comparison_util.h"
-#include "xla/hlo/analysis/tuple_points_to_analysis.h"
+#include "xla/hlo/analysis/hlo_alias_analysis.h"
 #include "xla/hlo/evaluator/hlo_evaluator_typed_visitor.h"
 #include "xla/hlo/ir/dfs_hlo_visitor_with_default.h"
 #include "xla/hlo/ir/hlo_casting_utils.h"
@@ -72,7 +72,7 @@ limitations under the License.
 #include "xla/primitive_util.h"
 #include "xla/service/gather_scatter_utils.h"
 #include "xla/service/hlo_module_config.h"
-#include "xla/service/logical_buffer.h"
+#include "xla/service/hlo_value.h"
 #include "xla/service/pattern_matcher.h"
 #include "xla/service/shape_inference.h"
 #include "xla/shape.h"
@@ -981,7 +981,7 @@ absl::StatusOr<Literal> HloEvaluator::Evaluate(
   // Reset evaluation state with the argument literals.
   ScopedEvaluateState evaluate_state(&state_, args);
 
-  tuple_points_to_analysis_cache_.reset();
+  alias_analysis_cache_.reset();
 
   // Re-seed RNG, either from the configuration's seed or a monotonic
   // per-evaluator seed (which prevents two evaluators from returning the same
@@ -1022,7 +1022,7 @@ absl::StatusOr<Literal> HloEvaluator::Evaluate(
     SetEvaluatedLiteralFor(substituted_instr, literal_value->Clone());
   }
 
-  tuple_points_to_analysis_cache_.reset();
+  alias_analysis_cache_.reset();
   auto enable_partial_evaluation_cleanup =
       absl::MakeCleanup([this] { enable_partial_evaluation_ = false; });
   enable_partial_evaluation_ = recursively_evaluate_nonconstant_operands;
@@ -1184,32 +1184,32 @@ absl::Status HloEvaluator::EvaluateParameterFromCallerArgument(
         ", which is not yet supported."));
   }
   if (computation_caller->opcode() == HloOpcode::kWhile) {
-    if (!analyses.tuple_points_to && !tuple_points_to_analysis_cache_) {
-      absl::StatusOr<std::unique_ptr<TuplePointsToAnalysis>> tuple_points_to =
-          TuplePointsToAnalysis::Run(parameter->GetModule());
-      if (!tuple_points_to.ok()) {
-        return absl::FailedPreconditionError(
-            "Failed to run TuplePointsToAnalysis.");
+    if (!analyses.alias_analysis && !alias_analysis_cache_) {
+      absl::StatusOr<std::unique_ptr<HloAliasAnalysis>> alias_analysis =
+          HloAliasAnalysis::Run(parameter->GetModule());
+      if (!alias_analysis.ok()) {
+        return absl::FailedPreconditionError("Failed to run HloAliasAnalysis.");
       }
-      tuple_points_to_analysis_cache_ = *std::move(tuple_points_to);
+      alias_analysis_cache_ = *std::move(alias_analysis);
     }
-    TuplePointsToAnalysis* tuple_points_to_analysis =
-        analyses.tuple_points_to != nullptr
-            ? analyses.tuple_points_to
-            : tuple_points_to_analysis_cache_.get();
+    HloAliasAnalysis* alias_analysis = analyses.alias_analysis != nullptr
+                                           ? analyses.alias_analysis
+                                           : alias_analysis_cache_.get();
 
     HloComputation* while_body = computation_caller->while_body();
-    ABSL_ASSIGN_OR_RETURN(
-        const LogicalBuffer* logical_buffer,
-        tuple_points_to_analysis->GetBufferDefinedAt(
-            while_body->parameter_instruction(parameter->parameter_number()),
-            shape_index));
-    const TuplePointsToAnalysis::BufferAliasVector& buffer_aliases =
-        tuple_points_to_analysis->GetBufferAliases(*logical_buffer);
+    int64_t param_num = parameter->parameter_number();
+    const HloValueSet& param_value_set =
+        alias_analysis->dataflow_analysis().GetValueSet(
+            while_body->parameter_instruction(param_num), shape_index);
+    const HloValueSet& root_value_set =
+        alias_analysis->dataflow_analysis().GetValueSet(
+            while_body->root_instruction(), shape_index);
+
     bool unchanged_in_return = false;
-    for (const BufferAlias& buffer_alias : buffer_aliases) {
-      if (buffer_alias.instruction() == while_body->root_instruction() &&
-          buffer_alias.index() == shape_index) {
+    if (root_value_set.values().size() == 1 &&
+        param_value_set.values().size() == 1) {
+      if (root_value_set.values()[0]->id() ==
+          param_value_set.values()[0]->id()) {
         unchanged_in_return = true;
       }
     }
